0b3f02c6acf31bda4e43f9ceda9bbd276b9f04dc
Melissa He <melissahe@ac.c4q.nyc>
2017-12-14 21:07:33 -0500

    Add review model

--- Collection-View-Exercises/Models/Review.swift
+++ Collection-View-Exercises/Models/Review.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct ReviewResultsWrapper {
+    let results: [Review]
+}
+
+struct Review: Codable {
+    let title: String
+    let multimedia: MultimediaWrapper?
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "display_title"
+        case multimedia
+    }
+    
+}
+
+struct MultimediaWrapper: Codable {
+    let link: String
+    
+    enum CodingKeys: String, CodingKey {
+        case link = "src"
+    }
+}
